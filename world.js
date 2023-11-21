198c4761934572bad97d48a952ec380d17a60bf5
Rojre Byfield <rojreb@gmail.com>
2023-11-20 22:53:11 -0500

    updated filies

--- world.js
+++ world.js
@@ -1,13 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
   let countryLookup = document.getElementById("lookup");
+  let cityLookup = document.getElementById("city");
 
   let resultDiv = document.getElementById("result");
 
+  // country
   countryLookup.addEventListener("click", function () {
     let country = encodeURIComponent(
       document.getElementById("country").value.trim()
     );
     fetch("world.php?country=" + country)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        resultDiv.innerHTML = response;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
+  // city
+  cityLookup.addEventListener("click", function () {
+    let country = encodeURIComponent(
+      document.getElementById("country").value.trim()
+    );
+    fetch("world.php?country=" + country + "&lookup=cities")
       .then((response) => {
         return response.json();
       })
--- world.php
+++ world.php
@@ -6,6 +6,32 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
+if (isset($_GET['lookup']) && $_GET['lookup'] === 'cities') {
+    $country = htmlspecialchars($_GET['country'], ENT_QUOTES, 'UTF-8');
+    $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$country%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $cityTable = '<table>
+    <tr>
+      <th>Name</th>
+     <th >District</th>
+    <th >Population</th>
+    </tr>';
+
+   foreach ($results as $row){
+
+    $cityTable .= '<tr>';
+    $cityTable .= '<td>' . $row['name'] . '</td>';
+    $cityTable .= '<td>' . $row['district'] . '</td>';
+    $cityTable .= '<td>' . $row['population'] . '</td>';
+    $cityTable .= '</tr>';
+   }
+   $cityTable .= '</table>';
+   echo json_encode($cityTable);
+   exit;
+    
+}
+
 // Check if 'country' parameter is set
 if(isset($_GET['country'])) {
      
